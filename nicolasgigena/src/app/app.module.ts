b6bb03faef96ef48433709133cfbd33a9cf44f66
poeticstew40 <nicolas_gigena@hotmail.es>
2022-09-19 18:35:36 -0300

    acerca de

--- nicolasgigena/src/app/app.module.ts
+++ nicolasgigena/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SocialComponent } from './components/social/social.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SocialComponent,
+    AcercaDeComponent,
   ],
   imports: [
     BrowserModule
