79e96be08751f52e7fdf3719f5dd1c42b4ce925b
poeticstew40 <nicolas_gigena@hotmail.es>
2022-10-24 13:02:56 -0300

    reparando hys

--- nicolasgigena/FrontEnd/src/app/app.module.ts
+++ nicolasgigena/FrontEnd/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { EditSkillComponent } from './components/hys/edit-skill.component';
+import { NewSkillComponent } from './components/hys/new-skill.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +39,9 @@ import { EditeducacionComponent } from './components/educacion/editeducacion.com
     NewExperienciaComponent,
     EditExperienciaComponent,
     NeweducacionComponent,
-    EditeducacionComponent
+    EditeducacionComponent,
+    EditSkillComponent,
+    NewSkillComponent
   ],
   imports: [
     BrowserModule,
--- nicolasgigena/FrontEnd/src/app/service/educacion.service.ts
+++ nicolasgigena/FrontEnd/src/app/service/educacion.service.ts
@@ -8,7 +8,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = 'https://bkndng.herokuapp.com/educacion/';
+  URL = environment.URL + 'educacion/';
 
   constructor(private httpClient : HttpClient) { }
 
